39c099e091c5926b7b9f5eb4e17bcbdf305b0c77
Hyeokjae Lee (Nagle) <leehyeokjae97@gmail.com>
2021-09-11 07:45:42 +0900

    ⚗️ Testing: PWA

--- sw.js
+++ sw.js
@@ -6,19 +6,17 @@ self.addEventListener("install", (e) => {
     caches
       .open(repositoryName)
       .then((cache) =>
-        cache.addAll(
-          make_default_path([
-            "",
-            "index.html",
-            "assets/js/install-handler.js",
-            "assets/js/main.js",
-            "assets/style/css/main.min.css",
-            "assets/icon/icon-192x129.png",
-            "assets/icon/icon-256x256.png",
-            "assets/icon/icon-384x384.png",
-            "assets/icon/icon-512x512.png",
-          ])
-        )
+        cache.addAll([
+          "/gititle/",
+          "/gititle/index.html",
+          "/gititle/assets/js/install-handler.js",
+          "/gititle/assets/js/main.js",
+          "/gititle/assets/style/css/main.min.css",
+          "/gititle/assets/icon/icon-192x129.png",
+          "/gititle/assets/icon/icon-256x256.png",
+          "/gititle/assets/icon/icon-384x384.png",
+          "/gititle/assets/icon/icon-512x512.png",
+        ])
       )
   );
 });
@@ -27,3 +25,14 @@ self.addEventListener("fetch", (e) => {
   console.log(e.request.url);
   e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
 });
+/*
+ "",
+            "index.html",
+            "assets/js/install-handler.js",
+            "assets/js/main.js",
+            "assets/style/css/main.min.css",
+            "assets/icon/icon-192x129.png",
+            "assets/icon/icon-256x256.png",
+            "assets/icon/icon-384x384.png",
+            "assets/icon/icon-512x512.png",
+*/
