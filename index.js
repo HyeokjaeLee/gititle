cfe3b7d48997d32c70814f55e123d4f69d807a54
Hyeokjae Lee (Nagle) <leehyeokjae97@gmail.com>
2021-09-11 07:49:20 +0900

    ⚗️ Testing: PWA

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const images = ["fox1", "fox2", "fox3", "fox4"];
+const imgElem = document.querySelector("img");
+
+function randomValueFromArray(array) {
+  const randomNo = Math.floor(Math.random() * array.length);
+  return array[randomNo];
+}
+
+setInterval(() => {
+  const randomChoice = randomValueFromArray(images);
+  imgElem.src = `images/${randomChoice}.jpg`;
+}, 2000);
+
 // Register service worker to control making site work offline
 
 if ("serviceWorker" in navigator) {
@@ -5,7 +18,9 @@ if ("serviceWorker" in navigator) {
     console.log("Service Worker Registered");
   });
 }
+
 // Code to handle install prompt on desktop
+
 let deferredPrompt;
 const addBtn = document.querySelector(".add-button");
 addBtn.style.display = "none";
--- sw.js
+++ sw.js
@@ -1,21 +1,17 @@
-const repositoryName = "gititle",
-  make_default_path = (paths) => paths.map((path) => `/${repositoryName}/${path}`);
-
 self.addEventListener("install", (e) => {
   e.waitUntil(
     caches
-      .open(repositoryName)
+      .open("fox-store")
       .then((cache) =>
         cache.addAll([
           "/gititle/",
           "/gititle/index.html",
-          "/gititle/assets/js/install-handler.js",
-          "/gititle/assets/js/main.js",
-          "/gititle/assets/style/css/main.min.css",
-          "/gititle/assets/icon/icon-192x129.png",
-          "/gititle/assets/icon/icon-256x256.png",
-          "/gititle/assets/icon/icon-384x384.png",
-          "/gititle/assets/icon/icon-512x512.png",
+          "/gititle/index.js",
+          "/gititle/style.css",
+          "/gititle/fox1.jpg",
+          "/gititle/fox2.jpg",
+          "/gititle/fox3.jpg",
+          "/gititle/fox4.jpg",
         ])
       )
   );
@@ -25,14 +21,3 @@ self.addEventListener("fetch", (e) => {
   console.log(e.request.url);
   e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
 });
-/*
- "",
-            "index.html",
-            "assets/js/install-handler.js",
-            "assets/js/main.js",
-            "assets/style/css/main.min.css",
-            "assets/icon/icon-192x129.png",
-            "assets/icon/icon-256x256.png",
-            "assets/icon/icon-384x384.png",
-            "assets/icon/icon-512x512.png",
-*/
