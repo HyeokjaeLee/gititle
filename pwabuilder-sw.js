cf386d38992ff425ea873fae4c4648352eb02283
Hyeokjae Lee (Nagle) <leehyeokjae97@gmail.com>
2021-09-10 19:20:12 +0900

    🩹 Fix: warnings related to PWA

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,8 +1,6 @@
-// This is the "Offline copy of assets" service worker
-
 const CACHE = "pwabuilder-offline";
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js");
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
@@ -11,8 +9,8 @@ self.addEventListener("message", (event) => {
 });
 
 workbox.routing.registerRoute(
-  new RegExp('/*'),
+  new RegExp("/*"),
   new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
+    cacheName: CACHE,
   })
 );
