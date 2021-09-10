e315f4f45cb558c6ea2a98e186481e214c7ab129
Hyeokjae Lee (Nagle) <leehyeokjae97@gmail.com>
2021-09-11 07:36:41 +0900

    🐛 Fix: path error

--- assets/js/install-handler.js
+++ assets/js/install-handler.js
@@ -1,3 +1,10 @@
+// Register service worker to control making site work offline
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/gititle/sw.js").then(() => {
+    console.log("Service Worker Registered");
+  });
+}
 // Code to handle install prompt on desktop
 let deferredPrompt;
 const addBtn = document.querySelector(".add-button");
